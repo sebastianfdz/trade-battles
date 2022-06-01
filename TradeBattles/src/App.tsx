016f21a3c6ee5ce0c6e5e89e492c62b3a6683df4
sebasfdz <fdz.seb@gmail.com>
2022-06-01 18:26:14 +0200

    Sign in screen UI implemented

--- TradeBattles/src/App.tsx
+++ TradeBattles/src/App.tsx
@@ -3,11 +3,13 @@ import {View, Text, StyleSheet} from 'react-native';
 import {theme} from './themes';
 import {NavigationContainer} from '@react-navigation/native';
 import {BottomTabsNavigator} from './screens/BottomTabs.navigator';
+import {Login} from './screens/Login.screen';
 
 export const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <BottomTabsNavigator />
+      <Login />
+      {/* <BottomTabsNavigator /> */}
     </NavigationContainer>
   );
 };
---
+++ TradeBattles/src/components/CustomInput.component.tsx
@@ -0,0 +1,43 @@
+import React, {SetStateAction} from 'react';
+import {StyleSheet, View, TextInput, useWindowDimensions} from 'react-native';
+import {theme} from '../themes';
+
+type CustomInputProps = {
+  value: string;
+  setValue: React.Dispatch<SetStateAction<any>>;
+  placeholder: string;
+  secureTextEntry?: boolean;
+};
+
+export const CustomInput: React.FC<CustomInputProps> = ({
+  value,
+  setValue,
+  placeholder,
+  secureTextEntry,
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        placeholder={placeholder}
+        style={styles.input}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.primary_grey,
+    width: 250,
+    height: 50,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    paddingHorizontal: 15,
+    borderRadius: 7,
+    marginVertical: 7,
+  },
+  input: {},
+});
