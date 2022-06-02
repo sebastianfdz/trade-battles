0eb678b6c5c3411f738916ced87141be8ffd0fdc
sebasfdz <fdz.seb@gmail.com>
2022-06-02 17:00:07 +0200

    Backend added battle, user and transaction's model and controller

--- TradeBattles/src/App.tsx
+++ TradeBattles/src/App.tsx
@@ -3,12 +3,14 @@ import {View, Text, StyleSheet} from 'react-native';
 import {theme} from './themes';
 import {NavigationContainer} from '@react-navigation/native';
 import {BottomTabsNavigator} from './screens/BottomTabs.navigator';
+import {LoginOnlySocial} from './screens/LoginOnlySocial.screen';
 import {Login} from './screens/Login.screen';
 
 export const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Login />
+      <LoginOnlySocial />
+      {/* <Login /> q */}
       {/* <BottomTabsNavigator /> */}
     </NavigationContainer>
   );
---
+++ TradeBattles/src/screens/LoginOnlySocial.screen.tsx
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
+import {CustomInput} from '../components/CustomInput.component';
+import {CustomButton} from '../components/CustomButton.component';
+import {theme} from '../themes';
+const googleImageSource = require('../../assets/images/Google_logo.png');
+const facebookImageSource = require('../../assets/images/Facebook_logo.png');
+const appleImageSource = require('../../assets/images/Apple_logo.png');
+
+const logoSrc = require('../../assets/images/Placeholder_logo.png');
+
+export const LoginOnlySocial: React.FC = () => {
+  const {height} = useWindowDimensions();
+
+  const onSignInWithGooglePressed = () => {
+    console.warn('Google Sign In');
+  };
+
+  const onSignInWithFacebookPressed = () => {
+    console.warn('Google Sign In');
+  };
+
+  const onSignInWithApplePressed = () => {
+    console.warn('Google Sign In');
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={logoSrc}
+          style={[styles.logo, {height: height * 0.3}]}
+          resizeMode="contain"
+        />
+
+        <CustomButton
+          text="Sign In with Google"
+          onPress={onSignInWithGooglePressed}
+          type="PRIMARY"
+          backgroundColor={theme.primary_grey}
+          icon={googleImageSource}
+        />
+        <CustomButton
+          text="Sign In with Facebook"
+          onPress={onSignInWithFacebookPressed}
+          type="PRIMARY"
+          backgroundColor={theme.primary_grey}
+          icon={facebookImageSource}
+        />
+        <CustomButton
+          text="Sign In with Apple"
+          onPress={onSignInWithApplePressed}
+          type="PRIMARY"
+          backgroundColor={theme.primary_grey}
+          icon={appleImageSource}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {alignItems: 'center', padding: 20, marginTop: 60},
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  },
+});
